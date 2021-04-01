5319afdeea4ac2c751e8b114a30bc14c7c1f2bc6
MansiVerma18 <mansi.verma_cs18@gla.ac.in>
2021-04-01 14:25:23 +0530

    commit

--- Teacher/Add_Quiz2.php
+++ Teacher/Add_Quiz2.php
@@ -25,43 +25,42 @@ include('Teacher_Structure.php');
                 
                 // echo $number[0];
                 for($i=1;$i<=$number[0];$i++){
-                    $query1=mysqli_query($conn,"insert into $name (id) values('$i')")
                 ?>
                 <div class="card my-3">
                 <div class="card-body bg-light"> 
                   <div class="my-2">                                      
-                    <select class="form-select" name="level"aria-label="Default select example">
+                    <select class="form-select" name="level<?php echo $i?>"aria-label="Default select example">
                       <option value="1">Easy</option>
                       <option value="2">Medium</option>
                       <option value="3">Hard</option>
                     </select>
                   </div>
                   <div class="col-md-12">
-                    <textarea class="form-control" name="question" placeholder="Write question here"></textarea>                   
+                    <textarea class="form-control" name="question<?php echo $i?>" placeholder="Write question here"></textarea>                   
                   </div>
                     <div class="input-group my-3">
                         <div class="input-group-text">
                           <input name="answer<?php echo $i?>" class="form-check-input mt-0" type="radio"  value="option1" aria-label="Radio button for following text input">
                         </div>
-                        <input type="text" id="floatingInputValue" name="option1"class="form-control" placeholder="Answer option 1"aria-label="Text input with radio button">
+                        <input type="text" id="floatingInputValue" name="option1<?php echo $i?>"class="form-control" placeholder="Answer option 1"aria-label="Text input with radio button">
                     </div>
                     <div class="input-group my-3">
                         <div class="input-group-text">
                           <input name="answer<?php echo $i?>"class="form-check-input mt-0" type="radio"   value="option2" aria-label="Radio button for following text input">
                         </div>
-                        <input type="text" id="floatingInputValue" name="option2"class="form-control" placeholder="Answer option 2"aria-label="Text input with radio button">
+                        <input type="text" id="floatingInputValue" name="option2<?php echo $i?>"class="form-control" placeholder="Answer option 2"aria-label="Text input with radio button">
                     </div>
                     <div class="input-group my-3">
                         <div class="input-group-text">
                           <input name="answer<?php echo $i?>"class="form-check-input mt-0" type="radio"   value="option3" aria-label="Radio button for following text input">
                         </div>
-                        <input type="text" id="floatingInputValue" name="option3"class="form-control" placeholder="Answer option 3"aria-label="Text input with radio button">
+                        <input type="text" id="floatingInputValue" name="option3<?php echo $i?>"class="form-control" placeholder="Answer option 3"aria-label="Text input with radio button">
                     </div>
                     <div class="input-group my-3">
                         <div class="input-group-text">
                           <input name="answer<?php echo $i?>"class="form-check-input mt-0" type="radio"  value="option4" aria-label="Radio button for following text input">
                         </div>
-                        <input type="text" class="form-control" name="option4"id="floatingInputValue" placeholder="Answer option 4"aria-label="Text input with radio button">
+                        <input type="text" class="form-control" name="option4<?php echo $i?>"id="floatingInputValue" placeholder="Answer option 4"aria-label="Text input with radio button">
                     </div>
                 </div>
                     </div>
@@ -95,46 +94,43 @@ include('Teacher_Structure.php');
 include("connection.php");
 if(isset($_POST['submit'])){
     $name=$_GET['QuizName'];
-    $level = $_POST['level'];
-    $question=$_POST['question'];
-    $option1=$_POST['option1'];
-    $option2=$_POST['option2'];
-    $option3=$_POST['option3'];
-    $option4=$_POST['option4'];
     $query=mysqli_query($connection,"select Questions from quiz where QuizName='$name'");
     $number=mysqli_fetch_array($query);
+
+    echo "<script> alert('$number[0]')</script>";
     for($i=1;$i<=$number[0];$i++){
+      $level = $_POST["level$i"];
+      $question=$_POST["question$i"];
+      $option1=$_POST["option1$i"];
+      $option2=$_POST["option2$i"];
+      $option3=$_POST["option3$i"];
+      $option4=$_POST["option4$i"];      
         $answer=$_POST["answer$i"];
-        if($answer ==option1){
-            $query=mysqli_query($conn,"insert into $name (Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question','$option1','$option1','$option2','$option3','$option4','$level')");
+        if($answer =="option1"){          
+            $query=mysqli_query($conn,"INSERT INTO $name(Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question', '$option1','$option1', '$option2', '$option3','$option4','$level')");
             if($query){
-                echo "<script> alert('Added Successfully');
-                window.location='Quiz.php';
-                </script>";
+              
             }
         }
-        else if($answer ==option2){
-            $query=mysqli_query($conn,"insert into $name (Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question','$option2','$option1','$option2','$option3','$option4','$level')");
+        else if($answer =="option2"){
+            $query=mysqli_query($conn,"INSERT INTO $name(Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question', '$option2','$option1', '$option2', '$option3','$option4','$level')");
             if($query){
-                echo "<script> alert('Added Successfully');
-                window.location='Quiz.php';</script>";
+              
             }
         }
-        else if($answer ==option3){
-            $query=mysqli_query($conn,"insert into $name (Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question','$option3','$option1','$option2','$option3','$option4','$level')");
+        else if($answer =="option3"){
+            $query=mysqli_query($conn,"INSERT INTO $name(Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question', '$option3','$option1', '$option2', '$option3','$option4','$level')");
             if($query){
-                echo "<script> alert('Added Successfully');
-                window.location='Quiz.php';</script>";
+              
             }
         }
-        else if($answer ==option4){
-            $query=mysqli_query($conn,"insert into $name (Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question','$option4','$option1','$option2','$option3','$option4','$level')");
+        else if($answer =="option4"){
+            $query=mysqli_query($conn,"INSERT INTO $name(Question, Answer, Option1, Option2, Option3, Option4, Level) VALUES ('$question', '$option4','$option1', '$option2', '$option3','$option4','$level')");
             if($query){
-                echo "<script> alert('Added Successfully');
-                window.location='Quiz.php';</script>";
+
             }
         }
-    }
-    
-}
-?>
+    }     echo "<script> alert('Added Successfully');
+  window.location='Quiz.php';</script>";
+  }
+    ?>

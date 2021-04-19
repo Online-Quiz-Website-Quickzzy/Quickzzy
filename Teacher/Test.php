1d4db506979eeae0208542c678bdee786f143e5f
MansiVerma18 <mansi.verma_cs18@gla.ac.in>
2021-04-20 02:40:47 +0530

    commit

--- Teacher/Test.php
+++ Teacher/Test.php
@@ -9,6 +9,34 @@ include('Teacher_Structure.php');
 ?>
 <main class="col-md mx-auto mt-2 ms-sm-auto col-lg-9">
         <div class="container text-center">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <p class="text-center"><strong >Important!</strong>           <button style="border:none;outline:none;background-color:transparent" class="close " data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true"><i class="fas fa-times-circle"></i></span>
+          </button></p>
+          <?php
+            include("connection.php");
+            $name=$_GET['QuizName'];  
+            $sname=$_SESSION['uname'];     
+            $query=mysqli_query($connection,"select Questions from quiz where QuizName='$name'");
+            $number=mysqli_fetch_array($query);
+        
+            $query=mysqli_query($connection,"select Right_marks from quiz where QuizName='$name'");
+            $right_marks=mysqli_fetch_array($query);
+            $right_marks=$right_marks[0];
+        
+            $query=mysqli_query($connection,"select Wrong_marks from quiz where QuizName='$name'");
+            $wrong_marks=mysqli_fetch_array($query);
+            $wrong_marks=$wrong_marks[0];
+          ?>
+          
+            <p>There are total <strong><?php echo$number[0]?></strong> questions.</p>
+            <p>For every correct attempt you will get increament of <strong><?php echo$right_marks[0]?></strong> marks.</p>
+            <p>For every incorrect attempt you will get decrease of <strong><?php echo$wrong_marks[0]?></strong> marks.</p>
+            <p class="text-center"><strong>All the best!!</strong></p>
+          
+
+          
+        </div>
             <h2><b>
             <?php
                 $name=$_GET['QuizName'];
@@ -86,6 +114,10 @@ include('Teacher_Structure.php');
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+  
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>
